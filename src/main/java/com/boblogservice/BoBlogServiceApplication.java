3c9e07df0a0062fd783360b67ecf1ca77c264b36
gimminjae <min356812@naver.com>
2023-11-03 00:51:24 +0900

    create project

---
+++ src/main/java/com/boblogservice/BoBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.boblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoBlogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boblogservice/BoBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoBlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
