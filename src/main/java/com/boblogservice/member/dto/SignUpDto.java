02c8d40ea8377d457f6bccff4809b4e249feacd9
gimminjae <min356812@naver.com>
2023-11-09 18:19:13 +0900

    회원가입 조건 변경

--- src/main/java/com/boblogservice/member/dto/SignUpDto.java
+++ src/main/java/com/boblogservice/member/dto/SignUpDto.java
@@ -1,5 +1,6 @@
 package com.boblogservice.member.dto;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Range;
 
@@ -20,13 +21,13 @@ public class SignUpDto {
     @Size(min = 5, max = 16, message = "아이디는 5 ~16자 이어야 합니다.")
     private String username;
     @NotNull(message = "닉네임은 필수항목입니다.")
-    @Size(min = 5, max = 16, message = "닉네임은 5 ~16자 이어야 합니다.")
+    @Pattern(regexp = "^[A-Za-z0-9가-힣]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
     @NotNull(message = "비밀번호는 필수항목입니다.")
-    @Size(min = 5, max = 16, message = "비밀번호는 5 ~16자 이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{5,16}$", message = "비밀번호는 영문, 숫자, 특수문자를 적어도 하나 이상 포함하는 5~16자의 문자열입니다.")
     private String password1;
     @NotNull(message = "비밀번호 확인은 필수항목입니다.")
-    @Size(min = 5, max = 16, message = "비밀번호 확인은 5 ~16자 이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{5,16}$", message = "비밀번호는 영문, 숫자, 특수문자를 적어도 하나 이상 포함하는 5~16자의 문자열입니다.")
     private String password2;
     private String memType;
 }
